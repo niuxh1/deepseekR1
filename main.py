c5802831e00551fb5e8c2d5e851d252e1b8017d7
Xuanhao Niu <150605048+niuxh1@users.noreply.github.com>
2025-03-06 12:06:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import random
+
+
+def api_call(question):
+    n= random.randint(10, 20)
+    for i in range(n):
+        j = i % 4
+        print("\r思考中"+j*".", end='', flush=True)
+        time.sleep(1)
+    print("\n服务器繁忙，请稍后再试")
+
+if __name__ == "__main__":
+    question = input("请输入问题：")
+    api_call(question)
